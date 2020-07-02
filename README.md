7961c4d92f6c85ba63cdc746e8fecc17472fb9dd
Tommy Swimmer <tommyswimmer@mail.com>
2020-07-01 18:22:17 -0600

    established library and eager_execution.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # raman-id
 Creation of a CNN using ResNet architecture to identify bacterial isolates. <br />
 
-Data from: <a href="https://github.com/csho33/bacteria-ID">csho33<a/>
+Data from: <a https://www.dropbox.com/sh/gmgduvzyl5tken6/AABtSWXWPjoUBkKyC2e7Ag6Da?dl=0">csho33<a/>
--- load_this.py
+++ load_this.py
@@ -1,2 +1,9 @@
-# Universal dataloader to ready Numpy arrays and csv files.
+# Universal dataloader to load .npy and .csv files.
+# Tommy Swimmer, Undergraduate Engineering Student
+# Fort Lewis College, 07/01/20
+
+import tensorflow as tf
+import numpy as np
+
+tf.enable_eager_execution()
 
